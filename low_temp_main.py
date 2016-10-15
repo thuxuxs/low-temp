cb5dd4a0daa0fa0e81d586ecaaa36cf86e7310f2
xuxs15@mails.tsinghua.edu.cn <xuxs15@mails.tsinghua.edu.cn>
2016-10-15 12:03:08 +0800

    change the size according to the type of machine

--- low_temp_main.py
+++ low_temp_main.py
@@ -18,7 +18,7 @@ class MainWindow(QMainWindow):
         self.create_menu()
 
     def create_instruments(self):
-        self.MDO=MDO_MainWindow(self)
+        self.MDO = MDO_MainWindow(self, GPIB='USB0::0x0699::0x0454::C021335::INSTR')
         self.AFG=AFG_MainWindow(self)
         self.MDO.move(0,20)
         self.AFG.move(0,20)
@@ -39,10 +39,12 @@ class MainWindow(QMainWindow):
     def show_AFG(self):
         self.AFG.setVisible(True)
         self.MDO.setVisible(False)
+        self.resize(800, 520)
 
     def show_MDO(self):
         self.MDO.setVisible(True)
         self.AFG.setVisible(False)
+        self.resize(1000, 520)
 
 if __name__=='__main__':
     MAIN_GUI=QApplication(sys.argv)
